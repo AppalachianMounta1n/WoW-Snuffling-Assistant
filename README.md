80bd02adacb9fa54b4ff21c412bb518776be66d4
Lily <ngross1015@gmail.com>
2024-09-09 16:47:15 -0400

    Guess it ain't possible, whoopsies

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
+# Apparently this might not even be possible because nodes are a backend thing and not UI, nevermind!
+
 # WoW-Snuffling-Assistant
 A simple World of Warcraft addon to make snuffling easier in The War Within by marking Disturbed Earth locations on your map/minimap.
 
-## This addon is not complete or functional, and this README will be updated when it has become functinal.
+## This addon is not complete or functional, and this README will be updated when it has become functional.
--- SnufflingAssistant/SnufflingAssistant.lua
+++ SnufflingAssistant/SnufflingAssistant.lua
@@ -1,7 +1,16 @@
 --print("'/dump GetBuildInfo()' will get you the current build info and interface version")
 
 local function OnZoneChangedNewArea(self, event)
-    print("Zone changed, finding Disturbed Earth")
+    print("Zone changed, finding Disturbed Earth nodes")
+
+    --Get the current zone
+    local currentZone = C_Map.GetBestMapForUnit("player")
+
+    --Find the Disturbed Earth node locations
+    local nodeLocations = C_AreaPoiInfo.GetAreaPOIForMap(currentZone)
+    for i, node in ipairs(nodeLocations) do
+        print(C_AreaPoiInfo.GetAreaPOIInfo(currentZone, node).name)
+    end
 end
 
 local zoneChangeNewAreaFrame = CreateFrame("Frame")
